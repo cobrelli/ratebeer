ce8aa9d8b1fdeb2e3f32ec7bd94a93d33709da88
Tomi Virtanen <cobrelli@gmail.com>
2014-01-24 12:52:22 +0200

    routesei, kaikkee

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -14,4 +14,10 @@ class RatingsController < ApplicationController
     #yllättävää ;)
     #redirect_to "http://www.cs.helsinki.fi"
   end
+
+  def destroy
+    rating = Rating.find(params[:id])
+    rating.delete
+    redirect_to ratings_path
+  end
 end
