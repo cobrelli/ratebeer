c9132fcb7950dbe54d309e59c0ed78296f159bed
Tomi Virtanen <cobrelli@gmail.com>
2014-01-24 14:59:23 +0200

    averagea saadetty, nakojaan piti erillinen metodi jo 4 tehtavassakin tehda :) no nyt on

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -5,4 +5,23 @@ class Beer < ActiveRecord::Base
 	def to_s
 		"#{name} #{brewery.name}"
 	end
+
+	def average_rating
+		if ratings.count == 0
+			return 0
+		end
+
+		str = "beer has "
+ 	
+ 		if ratings.count > 1
+ 			str += ratings.count.to_s + " ratings"
+ 		else
+ 			str += ratings.count.to_s + " rating"
+ 		end
+
+ 	    str += ", average "
+ 	    str += '%.1f' % (ratings.average 'score')
+ 	    
+ 	    return str
+	end
 end
--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,3 +1,23 @@
 class Brewery < ActiveRecord::Base
 	has_many :beers, :dependent => :destroy
+	has_many :ratings, :through => :beers
+
+	def average_rating
+		if ratings.count == 0
+			return 0
+		end
+
+		str = "Brewery has "
+ 	
+ 		if ratings.count > 1
+ 			str += ratings.count.to_s + " ratings"
+ 		else
+ 			str += ratings.count.to_s + " rating"
+ 		end
+
+ 	    str += ", average "
+ 	    str += '%.1f' % (ratings.average 'score')
+ 	    
+ 	    return str
+	end
 end
