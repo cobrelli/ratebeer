585a2dc25e4d6f6a0830ae80f1b81220a45619f9
Tomi Virtanen <cobrelli@gmail.com>
2014-01-30 11:39:09 +0200

    ratingit usereille

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140130063743) do
+ActiveRecord::Schema.define(version: 20140130093848) do
 
   create_table "beers", force: true do |t|
     t.string   "name"
