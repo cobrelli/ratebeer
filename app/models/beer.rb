c9223616f20ae63a6919eb688f6c8ef712469b3b
Tomi Virtanen <cobrelli@gmail.com>
2014-01-30 13:35:54 +0200

    asd

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -4,6 +4,7 @@ class Beer < ActiveRecord::Base
 	belongs_to :brewery
 	has_many :ratings, :dependent => :destroy
 
+	validates :name, presence => true
 	validates :score, numericality: { greater_than_or_equal_to: 1,
                                     less_than_or_equal_to: 50,
                                     only_integer: true }
