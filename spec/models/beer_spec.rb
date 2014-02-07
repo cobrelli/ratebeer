cadfe269eb2b052c09353e1a8d39ca7efe4b2ae8
Tomi Virtanen <cobrelli@gmail.com>
2014-02-07 17:25:30 +0200

    testejä

--- spec/models/beer_spec.rb
+++ spec/models/beer_spec.rb
@@ -1,5 +1,22 @@
 require 'spec_helper'
 
 describe Beer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+	describe "with set name and style" do
+		let(:beer){ Beer.create name:"Karhu III", style:"Lager" }
+
+		it "is saved" do
+			expect(beer).to be_valid
+  			expect(Beer.count).to eq(1)
+		end
+	end
+
+	describe "with name not set" do
+		let(:beer){ Beer.create style:"Esa" }
+
+		it "is not saved" do
+			expect(beer).not_to be_valid
+  			expect(Beer.count).to eq(0)
+		end
+	end
 end
