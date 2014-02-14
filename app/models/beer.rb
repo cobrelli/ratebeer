362bf083362accdc58fdb6a9cd89eaf1f852db0e
Tomi Virtanen <cobrelli@gmail.com>
2014-02-14 09:48:09 +0200

    styles

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,14 +1,15 @@
 class Beer < ActiveRecord::Base
 	include RatingAverage
 
+	belongs_to :style
 	belongs_to :brewery
 	has_many :ratings, :dependent => :destroy
 	has_many :raters, -> { uniq }, through: :ratings, source: :user, :dependent => :destroy
-
-
+	
 	#validates :name, presence => true
 	validates :name, presence: true
-	validates :style, presence: true
+	#validates :style, presence: true
+	validates_presence_of :style
 
 	def to_s
 		"#{name} #{brewery.name}"
